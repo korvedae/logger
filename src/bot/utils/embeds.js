c468104f0de77b8c1cca8d6f499f8eb451c1cf8e
TizzySaurus <tizzysaurus@gmail.com>
2024-04-14 18:03:39 +0100

    Add discriminator change to missed file

--- src/bot/utils/embeds.js
+++ src/bot/utils/embeds.js
@@ -1,13 +1,13 @@
 module.exports = {
   getEmbedFooter (user) {
     return {
-      text: `${user.username}#${user.discriminator}`,
+      text: `${user.username}${user.discriminator === '0' ? '' : `#${user.discriminator}`}`,
       icon_url: user.dynamicAvatarURL(null, 64)
     }
   },
   getAuthorField (user) {
     return {
-      name: `${user.username}#${user.discriminator}`,
+      name: `${user.username}${user.discriminator === '0' ? '' : `#${user.discriminator}`}`,
       icon_url: user.avatarURL
     }
   }
